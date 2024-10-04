67386248f8789f2166711fdcc81fde717421b70e
TE22D-Kindstedt-Leo <leo.kindstedt@elev.ga.ntig.se>
2024-10-04 12:45:21 +0200

    knsda

--- Tamagochi/Class.cs
+++ Tamagochi/Class.cs
@@ -1,8 +1,75 @@
 using System;
+using System.Collections.Generic;
 
-namespace Tamagochi;
-
-public class Class
+public class Tamagotchi
 {
+  private int hunger;
+  private int boredom;
+  private List<string> words = new List<string>() {"Knas"};
+  private bool Health;
+  public string namn;
 
+  public Tamagotchi()
+  {
+    Health = true;
+  }
+
+  public void Feed()
+  {
+    if (hunger > 0)
+    {
+      Console.WriteLine($" [{namn}] blir matat och är inte lika hungrig");
+      hunger = new Random().Next(2,5);
+    }
+    else if (hunger == 0)
+    {
+      hunger = 0;
+      Console.WriteLine($"{namn} är redan mät");
+    }
+  }
+
+  public void Hi()
+  {
+    int wordNum = Random.Shared.Next(words.Count);
+    Console.WriteLine($" [{namn}] says: {words[wordNum]}");
+    ReduceBoredom();
+  }
+
+  public void Teach(string word)
+  {
+    Console.WriteLine($" [{namn}] learns: {word}");
+    words.Add(word);
+    ReduceBoredom();
+  }
+
+  public void Tick()
+  {
+    hunger++;
+    boredom++;
+    if (hunger > 10 || boredom > 10)
+    {
+      Health = false;
+    }
+  }
+
+  public void PrintStats()
+  {
+    Console.WriteLine($"Namn: {namn} || Mat: {hunger}/10 || Boredom: {boredom}/10 || Vocabulary: {words.Count} words");
+  }
+
+  public bool GetAlive()
+  {
+    return Health;
+  }
+
+  public void ReduceBoredom()
+  {
+    Console.WriteLine($" [{namn}] is now less bored!");
+
+    boredom -= 2;
+    if (boredom < 0)
+    {
+      boredom = 0;
+    }
+  }
 }
--- Tamagochi/Program.cs
+++ Tamagochi/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿
+Console.WriteLine("Välkomen till Filur");
+
+Tamagotchi filur = new Tamagotchi();
+
+Console.WriteLine("Välj namn på din filur!");
+filur.namn = Console.ReadLine();
+
+Console.WriteLine($"{filur.namn} är ett fantastiskt namn");
+
+while (filur.GetAlive() == true)
+{
+  Console.Clear();
+  filur.PrintStats();
+  Console.WriteLine("Vad ska filuren göra");
+  Console.WriteLine($"1. Lär {filur.namn} ett nytt ord");
+  Console.WriteLine($"2. Prata med {filur.namn}");
+  Console.WriteLine($"3. Mata {filur.namn}");
+  Console.WriteLine($"4. Gör inget");
+
+  string Vad = Console.ReadLine();
+  if (Vad == "1")
+  {
+    Console.WriteLine("What word?");
+    string word = Console.ReadLine();
+    filur.Teach(word);
+  }
+  if (Vad == "2")
+  {
+    filur.Hi();
+  }
+  if (Vad == "3")
+  {
+    filur.Feed();
+  }
+  else
+  {
+    Console.WriteLine("Doing nothing...");
+  }
+  filur.Tick();
+  Console.WriteLine("Press Enter to continue");
+  Console.ReadLine();
+}
+
+Console.WriteLine($"OH NO! {filur.namn} is dead!");
+Console.WriteLine("Press ENTER to quit");
+Console.ReadLine();
